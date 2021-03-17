18dc8a1c11763ce94fdd9e58a0f42591885e5170
beefviper <beef.viper@gmail.com>
2021-03-16 23:30:57 -0400

    Added #ifdef for _MSC_VER to #define aliases for strncasecmp and strcasemp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,14 @@
 #include "system.h"
 #include "video.h"
 
+
+#ifdef _MSC_VER 
+// not #if defined(_WIN32) || defined(_WIN64)
+// because we have strncasecmp in mingw
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#endif
+
 #ifdef __SWITCH__
 #include <switch.h>
 #endif
